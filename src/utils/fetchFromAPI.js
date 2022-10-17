7f6bb1bd4199b52d4e5f5f64c1088538c7b307d3
bikalpasharma <bikalpasharma7@gmail.com>
2022-10-17 15:53:58 +0545

    YT Clone using rapid API

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'f5f25b6c1dmsh889ea78846bcfe4p1e3184jsnd6b4ff4ca9af',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
